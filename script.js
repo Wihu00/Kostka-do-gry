b000be5ffb899096f3ef7ea6267a77e72e1e712d
Wihu00 <34309272+Wihu00@users.noreply.github.com>
2021-11-12 13:57:22 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,12 +1,21 @@
-var cube = document.querySelector('.cube');
-var currentClass = '';
+const btn = document.querySelector('#random');
+btn.onclick = changeSide;
 
 function changeSide() {
-  var random = Math.floor(Math.random() * (7 - 1)) + 1;
-  var showClass = 'show-' + random;
-  if ( currentClass ) {
-    cube.classList.remove( currentClass );
-  }
-  cube.classList.add( showClass );
-  currentClass = showClass;
+  let random = random_number();
+  let face;
+
+  if(random == 1) face = "rotateY(0deg)";
+  else if( random == 2) face = "rotateY(-90deg)";
+  else if (random == 3) face = "rotateY(-180deg)";
+  else if (random == 4) face = "rotateY(-90deg)";
+  else if (random == 5) face = "rotateX(-90deg)";
+  else if (random == 6) face = "rotateX(90deg)";
+  else alert('error');
+
+  document.getElementById("cube").style.transform = face;
+}
+
+function random_number() {
+  return Math.floor(Math.random() * (7 - 1)) + 1;
 }
